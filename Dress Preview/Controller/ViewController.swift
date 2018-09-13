71e200dddcce389f7c4f211adc07bf7818c0f5c1
KungaK <s3588773@student.rmit.edu.au>
2018-09-13 22:23:10 +1000

    Working simulation of application. Clean version

--- Dress Preview/Controller/ViewController.swift
+++ Dress Preview/Controller/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionViewFlow: UICollectionViewFlowLayout!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
