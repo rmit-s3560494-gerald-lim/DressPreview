de606ed8918e3c94e60690d60941cb486c4d5c07
KungaK <s3588773@student.rmit.edu.au>
2018-09-17 12:19:52 +1000

    Details View done. Linked to Browse View

---
+++ Dress Preview/Controller/DetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.swift
+//  Dress Preview
+//
+//  Created by Kunga Kartung on 17/9/18.
+//  Copyright © 2018 🍆. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+}
