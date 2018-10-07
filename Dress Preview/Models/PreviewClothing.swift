a90952a61373c9c1f68f71c90e8452f51de0ef15
KungaK <s3588773@student.rmit.edu.au>
2018-10-07 20:28:14 +1100

    Preview to Detail Scene. Ebay image clickable and links to items ebay url in safari.

---
+++ Dress Preview/Models/PreviewClothing.swift
@@ -0,0 +1,15 @@
+//
+//  PreviewClothing.swift
+//  Dress Preview
+//
+//  Created by Kunga Kartung on 6/10/18.
+//  Copyright © 2018 🍆. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct PreviewClothing: Codable {
+    let imageArray: [String] = ["1.jpeg","2.jpeg","3.jpeg","4.jpeg","5.jpeg"]
+    let imageArrayTop: [String] = ["1top.jpeg","2top.jpeg","3top.jpeg","4top.jpeg","5top.jpeg"]
+}
